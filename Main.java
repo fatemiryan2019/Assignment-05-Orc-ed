bf4f7240ea23418857be2d068b391761842541f4
fatemiryan2019 <42677922+fatemiryan2019@users.noreply.github.com>
2018-10-02 08:30:09 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,20 +4,66 @@ public class Main {
         int max = 40 - str;
         int range = max - 10;
         int agi = (int) (Math.random() * range) + 10;
-        int smt = 50 - str - agi;
-        int dam = 5 * smt;
+        int intel = 50 - str - agi;
+        // intel = 19;
+        //System.out.println(smt);
+        int dam = 5 * intel;
         int health = 1000;
         double mod;
-        for (int i = 0; i <= 10; i++) {
-            mod = dam * (Math.random() * (1.5 - .5) + .5);
-            health -= mod;
-            if (health <= 0){
-                System.out.println("Success");
-                break;
+        mod = dam * (Math.random() * (1.5 - .5) + .5);
+        health -= mod;
+        if (health <= 0){
+            System.out.println("Success");
             }
-            if (i == 10 && !(health <= 0)){
-                System.out.println("Failure");
-            }
+        mod = dam * (Math.random() * (1.5 - .5) + .5);
+        health -= mod;
+        if (health <= 0) {
+            System.out.println("Success");
+
         }
+        mod = dam * (Math.random() * (1.5 - .5) + .5);
+        health -= mod;
+        if (health <= 0) {
+            System.out.println("Success");
+        }
+        mod = dam * (Math.random() * (1.5 - .5) + .5);
+        health -= mod;
+        if (health <= 0) {
+            System.out.println("Success");
+        }
+        mod = dam * (Math.random() * (1.5 - .5) + .5);
+        health -= mod;
+        if (health <= 0) {
+            System.out.println("Success");
+        }
+        mod = dam * (Math.random() * (1.5 - .5) + .5);
+        health -= mod;
+        if (health <= 0) {
+            System.out.println("Success");
+        }
+        mod = dam * (Math.random() * (1.5 - .5) + .5);
+        health -= mod;
+        if (health <= 0) {
+            System.out.println("Success");
+        }
+        mod = dam * (Math.random() * (1.5 - .5) + .5);
+        health -= mod;
+        if (health <= 0) {
+            System.out.println("Success");
+        }
+        mod = dam * (Math.random() * (1.5 - .5) + .5);
+        health -= mod;
+        if (health <= 0) {
+            System.out.println("Success");
+        }
+        mod = dam * (Math.random() * (1.5 - .5) + .5);
+        health -= mod;
+        if (health <= 0) {
+            System.out.println("Success");
+        }
+        if (health >= 0){
+            System.out.println("Failure");
+        }
+        System.out.println("\nFor 60-70% success rate, intelligence should be at least 19\nFor the same success rate with an intelligence level of 10, the base damage should be 10");
     }
 }
